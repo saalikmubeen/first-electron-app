e55e9619352f6c9ec39d6f1bdaf713f9a49fcc1d
saalik123 <63722568+saalik123@users.noreply.github.com>
2022-03-11 20:06:54 +0530

    coded addTodo window

--- index.js
+++ index.js
@@ -1,21 +1,57 @@
-const {app, BrowserWindow} = require("electron");
+const { app, BrowserWindow, Menu } = require("electron");
 
+const isMac = process.platform === "darwin";
 
 let mainWindow;
 
-const createWindow = () => { 
+const createWindow = () => {
     // Main Window -> Browser + Node JS
     mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 1000,
+        height: 800,
     });
 
+    mainWindow.on("close", () => {
+        app.quit();
+    })
 
-    mainWindow.loadURL(`file://${__dirname}/main.html`)
+    mainWindow.loadURL(`file://${__dirname}/main.html`);
     // mainWindow.loadFile("main.html");
 };
 
+let addWindow;
+const createAddWindow = () => {
+    addWindow = new BrowserWindow({
+        width: 500,
+        height: 300,
+        title: "Add New Todo",
+    });
+
+    addWindow.loadFile("add.html");
+};
+
+const menuTemplate = [
+    {
+        label: "File",
+        submenu: [
+            { label: "Add Todo", click: createAddWindow },
+            {
+                label: "Quit",
+                click: () => app.quit(),
+                accelerator: "CmdOrCtrl+Q",
+                // accelerator : isMac ? "Command+Q" : "Ctrl+Q"
+            },
+        ],
+    },
+];
+
+if (isMac) {
+    menuTemplate.unshift({});
+}
 
 app.on("ready", () => {
     createWindow();
-})
+
+    const menu = Menu.buildFromTemplate(menuTemplate);
+    Menu.setApplicationMenu(menu);
+});
